751ee05d3cae04e3e73c31d93986a5a3bee39f2e
JuanJoNavC <jjnavarrete008@gmail.com>
2025-06-04 23:41:22 -0500

    Route chat

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import PersonaEditView from '../views/PersonaEditView.vue'
 import FichaListView from '@/views/Ficha/FichaListView.vue'
 import FichaCreateWithPersona from '@/views/Ficha/FichaCreateWithPersona.vue'
 import FichaEditView from '@/views/Ficha/FichaEditView.vue'
-import ChatBut from '@/views/ChatBot.vue'
+import ChatBot from '@/views/ChatBot.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +21,7 @@ const router = createRouter({
     { path: '/fichas', component: FichaListView},
     { path: '/fichas/crear/:id', component: FichaCreateWithPersona, props: true}, //Create a Ficha With Persona id
     { path: '/fichas/editar/:id/:ficha', component: FichaEditView},
-    { path: '/chat', component: ChatBut}
+    { path: '/chat', component: ChatBot}
 
   ],
 })
